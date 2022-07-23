54b90beb7230b774ec6e98fe8a757a7993776bf7
DeltaByte <thedeltabyte@gmail.com>
2022-07-23 19:30:27 +0000

    feat: lambda deployment package palceholder

--- infrastructure/modules/lambda-function/lambda.tf
+++ infrastructure/modules/lambda-function/lambda.tf
@@ -1,3 +1,6 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Function
+# ----------------------------------------------------------------------------------------------------------------------
 resource "aws_lambda_function" "default" {
   function_name = var.name
   role          = aws_iam_role.lambda_execution.arn
@@ -15,8 +18,15 @@ resource "aws_lambda_function" "default" {
   environment {
     variables = var.environment_variables
   }
+
+  depends_on = [
+    aws_s3_object.placeholder
+  ]
 }
 
+# ----------------------------------------------------------------------------------------------------------------------
+# Permssion
+# ----------------------------------------------------------------------------------------------------------------------
 resource "aws_iam_role" "lambda_execution" {
   name               = var.name
   path               = var.role_path
@@ -31,5 +41,32 @@ data "aws_iam_policy_document" "lambda_assume" {
       type        = "Service"
       identifiers = ["lambda.amazonaws.com"]
     }
+  }
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Placeholder object (race condition when deploying for the first time)
+# ----------------------------------------------------------------------------------------------------------------------
+resource "aws_s3_object" "placeholder" {
+  bucket = var.source_bucket
+  key    = coalesce(var.source_key, "${var.name}.zip")
+
+  content_type = "application/zip"
+  content_base64 = join("", [
+    "UEsDBAoAAAAAABB/91TtxOLVFgAAABYAAAAJABwAYm9vdHN0cm",
+    "FwVVQJAAMvGtxiLhrcYnV4CwABBOgDAAAE6AMAAFRoaXMgaXMg",
+    "YSBwbGFjZWhvbGRlci5QSwECHgMKAAAAAAAQf/dU7cTi1RYAAA",
+    "AWAAAACQAYAAAAAAABAAAAtIEAAAAAYm9vdHN0cmFwVVQFAAMv",
+    "GtxidXgLAAEE6AMAAAToAwAAUEsFBgAAAAABAAEATwAAAFkAAA",
+    "AAAA=="
+  ])
+
+  lifecycle {
+    ignore_changes = [
+      etag,
+      tags,
+      tags_all,
+      version_id
+    ]
   }
 }
